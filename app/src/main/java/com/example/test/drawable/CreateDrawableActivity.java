07055171780c3367a075ece35b9cec8d3fd8b0b1
1812821235 <1812821235@qq.com>
2022-11-21 14:25:18 +0800

    动态生成bitmap，下注砝码

--- app/src/main/java/com/example/test/drawable/CreateDrawableActivity.java
+++ app/src/main/java/com/example/test/drawable/CreateDrawableActivity.java
@@ -1,8 +1,12 @@
 package com.example.test.drawable;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +19,14 @@ import com.example.test.R;
 public class CreateDrawableActivity extends AppCompatActivity {
 
     private TextView tvTest;
+    private ImageView imgvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_drawable);
         tvTest = findViewById(R.id.tv_test);
+        imgvTest = findViewById(R.id.imgv_test);
     }
 
     /**
@@ -32,7 +38,13 @@ public class CreateDrawableActivity extends AppCompatActivity {
     }
 
     public void onTest2(View v) {
-        tvTest.getBackground().setLevel(1);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 1;
+        Bitmap b1 = BitmapFactory.decodeResource(getResources(), R.drawable.ic_bet, options);
+        Bitmap b2 = BitmapFactory.decodeResource(getResources(), R.drawable.ic_bet_0, options);
+        Bitmap bt = BitmapUtils.createBet(this, 2);
+//        imgvTest.setImageBitmap(bt);
+        imgvTest.setBackground( new BitmapDrawable(getResources(), bt));
     }
 
     public void onTest3(View v) {
